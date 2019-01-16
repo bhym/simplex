b85b02e51ee2757f2fe20b0f22577fc4935c2f70
PaulFremont3 <46523274+PaulFremont3@users.noreply.github.com>
2019-01-16 12:22:31 +0100

    Evolution_kimura
    Functions to evolve DNA sequence
    - Creating random DNA sequences
    - Evolve these sequences according to kimura model

---
+++ scripts/evolution_kimura.R
@@ -0,0 +1,33 @@
+#function to create random DNA sequence
+randDNA = function(n){
+  paste(sample(c("A","T","C","G"), n, replace=TRUE), collapse="")
+}
+
+#evolution of a DNA sequence according to kimura model
+# Transition rate
+Alpha=(1/2)*10^-6
+# Transversion rate
+Beta=(1/4)*10^-6
+evolution_kimura<-function(sequence, t){
+  random_mut <- runif(n = nchar(sequence), 0, 1)
+  sequence_new <- sequence
+  p0 = 0.25*(1+exp(-4*Beta*t)+2*exp(-2*(Alpha+Beta)*t))
+  p1 = 0.25*(1-exp(-4*Beta*t))
+  p2 = 0.25*(1+exp(-4*Beta*t)-2*exp(-2*(Alpha+Beta)*t))
+  substitution_mat <- matrix(c(p0, p1, p1, p2, p1, p0, p2, p1, p1, p2, p0, p1, p2, p1, p1, p0), 
+                             ncol=4, nrow=4, byrow = T)
+  letters_dna <- c("A","T","C","G")
+  mutation_sites <- which(random_mut<1-p0)
+  for (u in mutation_sites){
+    vector <- match(letters_dna, substr(sequence, u,u), nomatch = 0)
+    probability_vector <- vector%*%substitution_mat-random_mut[u]
+    new_letters <- which(probability_vector == min(probability_vector))
+    if (length(new_letters)==2){
+      new_letter <- new_letters[round(runif(1, 1, 2))]
+    } else{
+      new_letter <- new_letters
+    }
+    substr(sequence_new, u,u) <- letters_dna[new_letter]
+  }
+  return(c(sequence_new, length(mutation_sites)))
+}
